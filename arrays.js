1f4e8b487a93b885c596a16a2076506a2688fb10
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-12 03:42:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,7 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift()
 }
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
